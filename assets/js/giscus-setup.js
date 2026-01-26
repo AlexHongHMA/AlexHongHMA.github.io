3573d7d1901a63782bd1fae73ea788afefbb14c3
AlexHongHMA <137489429+AlexHongHMA@users.noreply.github.com>
2026-01-26 08:28:43 +0000

    Deploying to gh-pages from @ AlexHongHMA/AlexHongHMA.github.io@c4cbe9f9d47a1b4499e661f00b53a28b479a78c1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AlexHongHMA/my-portfolio-website",
+    "data-repo": "AlexHongHMA/AlexHongHMA.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
